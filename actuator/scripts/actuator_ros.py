e06304a4107cc80a7ba962ec0ddc5230a09220a9
13290180869 <284471242@qq.com>
2023-05-14 08:51:21 +0800

    提高请求失败的请求频率，将膨胀行为均添加在两个代价地图中

--- actuator/scripts/actuator_ros.py
+++ actuator/scripts/actuator_ros.py
@@ -99,7 +99,6 @@ class CarActuator(object):
                 self.status = 4
             elif self.status == 3:
                 rospy.loginfo("请求失败")
-                rospy.sleep(5)
                 self.actuator_ask_newtarget()
 
             # 取药相关
