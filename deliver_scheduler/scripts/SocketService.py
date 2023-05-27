f1894de9365c119a280b4f3514a6eda6cd0787b6
罗宇峻 <am.luoyj@outlook.com>
2023-05-27 20:04:03 +0800

    scheduler现在在需要修改药物刷新时间时，将等待固定时间后自动修改刷新时间

--- deliver_scheduler/scripts/SocketService.py
+++ deliver_scheduler/scripts/SocketService.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import socket
 import json
+import socket
+import time
 
 
 class SocketServiceMaster(object):
-    def __init__(self, slave_addr, slave_port):
+    def __init__(self, slave_addr, slave_port, use_socket=False, time_to_return=5):
         self.slave_addr = slave_addr
         self.slave_port = slave_port
         self.socket = None
 
+        # 如果不采用socket,wait_for_service将立即返回,call方法将等待time_to_return秒后返回
+        self.use_socket = use_socket
+        self.time_to_return = time_to_return
+
     def wait_for_service(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.slave_addr, self.slave_port))
+        if self.use_socket:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.slave_addr, self.slave_port))
 
     def call(self, need=0):
+        if not self.use_socket:
+            time.sleep(self.time_to_return)
+            return
+
+        # 使用socket
         if not self.socket:
             raise RuntimeError("Service not connected. Call 'wait_for_service' first.")
 
